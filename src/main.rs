3ff0a148676c88bc671212988095fbbe9a8861e6
ShadoWing1 <oyungemer14@gmail.com>
2023-01-23 13:27:24 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, Write};
 
 fn input(input_entry: &str) -> String {
-    let mut input = String::new();
+    let mut input: String = String::new();
     print!("{}: ", input_entry);
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut input).unwrap(); // input input
@@ -11,12 +11,11 @@ fn input(input_entry: &str) -> String {
 
 fn main()
 {
-    let user_name = input("name");
-    let user_age = input("age");
-    let user_status = input("status");
+    let user_name: String = input("name");
+    let user_age: String = input("age");
+    let user_status: String = input("status");
 
-    let _num = user_age.trim().parse::<i32>().expect("Lütfen sayı giriniz");
+    let _num: i32 = user_age.trim().parse::<i32>().expect("Lütfen sayı giriniz");
 
     println!("Hello {}.Your age {} and your status {}.", user_name ,_num ,user_status);
-
 }
